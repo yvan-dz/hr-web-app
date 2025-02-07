2057b4a7472952bed461c733c982e64381c5fff2
yvan-dz <yvandzefak1@gmail.com>
2025-02-07 02:12:54 +0100

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SalaryPackageComponent } from './salary-package/salary-package.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
